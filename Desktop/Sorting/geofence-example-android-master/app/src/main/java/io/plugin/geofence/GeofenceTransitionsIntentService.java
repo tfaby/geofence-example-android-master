f26726fd265e7e041cad7949ba9d84dacbe3733c
Tyler Faby <tyler.faby@gmail.com>
2017-08-13 15:35:15 -0400

    Initial Upload Refresh

---
+++ Desktop/Sorting/geofence-example-android-master/app/src/main/java/io/plugin/geofence/GeofenceTransitionsIntentService.java
@@ -0,0 +1,76 @@
+package io.plugin.geofence;
+
+import android.app.IntentService;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.support.v7.app.NotificationCompat;
+import android.text.TextUtils;
+import android.util.Log;
+
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.GeofenceStatusCodes;
+import com.google.android.gms.location.GeofencingEvent;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GeofenceTransitionsIntentService extends IntentService {
+    protected static final String TAG = "GeofenceTransitionsIS";
+
+    public GeofenceTransitionsIntentService() {
+        super(TAG);  // use TAG to name the IntentService worker thread
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        GeofencingEvent event = GeofencingEvent.fromIntent(intent);
+        if (event.hasError()) {
+            Log.e(TAG, "GeofencingEvent Error: " + event.getErrorCode());
+            return;
+        }
+        String description = getGeofenceTransitionDetails(event);
+        sendNotification(description);
+        // Add in Bluetooth command
+    }
+
+    private static String getGeofenceTransitionDetails(GeofencingEvent event) {
+        String transitionString =
+                GeofenceStatusCodes.getStatusCodeString(event.getGeofenceTransition());
+        List triggeringIDs = new ArrayList();
+        for (Geofence geofence : event.getTriggeringGeofences()) {
+            triggeringIDs.add(geofence.getRequestId());
+        }
+        return String.format("%s: %s", transitionString, TextUtils.join(", ", triggeringIDs));
+    }
+
+    private void sendNotification(String notificationDetails) {
+        // Create an explicit content Intent that starts MainActivity.
+        Intent notificationIntent = new Intent(getApplicationContext(), MainActivity.class);
+
+        // Get a PendingIntent containing the entire back stack.
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        stackBuilder.addParentStack(MainActivity.class).addNextIntent(notificationIntent);
+        PendingIntent notificationPendingIntent =
+                stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        // Get a notification builder that's compatible with platform versions >= 4
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+
+        // Define the notification settings.
+        builder.setColor(Color.DKGRAY)
+                .setContentTitle(notificationDetails)
+                .setContentText("This is a geofence notification.")
+                .setContentIntent(notificationPendingIntent)
+                .setSmallIcon(R.drawable.geofence_logo)
+                .setAutoCancel(true);
+
+        // Fire and notify the built Notification.
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(0, builder.build());
+    }
+}
